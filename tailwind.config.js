ed9168672661dc3f51657f92577ee0a84436a7b3
fhnsamin <fhnislam@gmail.com>
2024-09-23 18:54:00 +0600

    Made nav and buttons

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#B4F461",
+        "text-main": "#111111",
+        "text-sub": "#111111b3",
+      },
+    },
+  },
+  plugins: [],
+};
